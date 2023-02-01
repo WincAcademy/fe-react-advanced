0ef6ec1c0c71855f8c8090b50c537fc0c2164721
Mark van der Laan <mrkvdl@gmail.com>
2023-02-01 11:08:12 +0100

    add: fetching_data

---
+++ fetching_data/solution/solution/src/App.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import { UserDetail } from './UserDetail';
+
+const App = () => {
+	const [users, setUsers] = useState([]);
+	const [selectedUser, setSelectedUser] = useState(null);
+
+	useEffect(() => {
+		const fetchUsers = async () => {
+			const response = await fetch(
+				'https://jsonplaceholder.typicode.com/users',
+			);
+			const friends = await response.json();
+			setUsers(friends);
+		};
+		fetchUsers();
+	}, []);
+
+	return (
+		<div className="App">
+			<h1>React Hooks exercise starter</h1>
+			<ul>
+				{users.map((user) => (
+					<li onClick={() => setSelectedUser(user)} key={user.id}>
+						{user.name}
+					</li>
+				))}
+			</ul>
+			{selectedUser && <UserDetail user={selectedUser} />}
+		</div>
+	);
+};
+
+export default App;

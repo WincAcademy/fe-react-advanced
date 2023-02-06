fa0d94957f64fe3077b4fb1e86daefb01ef1f71c
Pieter Stokkink <118258379+pieter-winc@users.noreply.github.com>
2023-02-06 17:07:49 +0100

    add sending data solution

--- exercise_routing/starter/src/main.jsx
+++ exercise_routing/starter/src/main.jsx
@@ -1,50 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  NewPost,
-  action as createPost,
-  loader as newPostLoader,
-} from "./NewPost";
-import { Post, loader as postLoader, action as addComment } from "./Post";
-import { PostList, loader as postListLoader } from "./PostList";
-import { Root } from "./Root";
-import { User, loader as userLoader } from "./User";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
 import "./index.css";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      {
-        path: "/",
-        element: <PostList />,
-        loader: postListLoader,
-      },
-      {
-        path: "/post/:postId",
-        element: <Post />,
-        loader: postLoader,
-        action: addComment,
-      },
-      {
-        path: "/user/:userId",
-        element: <User />,
-        loader: userLoader,
-      },
-      {
-        path: "/post/new",
-        element: <NewPost />,
-        action: createPost,
-        loader: newPostLoader,
-      },
-    ],
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("app")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );

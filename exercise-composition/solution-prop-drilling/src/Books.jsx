f36a28d7e01575cbefd165d0792ea72995832cd0
Pieter Stokkink <118258379+pieter-winc@users.noreply.github.com>
2023-02-11 11:36:10 +0100

    expand prop-drilling solution

--- exercise-composition/solution-prop-drilling/src/Books.jsx
+++ exercise-composition/solution-prop-drilling/src/Books.jsx
@@ -3,7 +3,7 @@ import { Category } from "./Category";
 export const Books = ({ books, borrowBook, returnBook }) => {
   return (
     <>
-      <h2>Books:</h2>
+      <h2>Books ({books.length}):</h2>
       <Category
         title="Programming"
         books={books.filter((book) => book.category === "programming")}
--- exercise-composition/solution-prop-drilling/src/Category.jsx
+++ exercise-composition/solution-prop-drilling/src/Category.jsx
@@ -3,7 +3,9 @@ import { Book } from "./Book";
 export const Category = ({ title, books, borrowBook, returnBook }) => {
   return (
     <>
-      <h3>{title}</h3>
+      <h3>
+        {title} ({books.length}):
+      </h3>
       {books.map((book) => (
         <Book
           key={book.id}

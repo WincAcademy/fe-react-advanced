4d889e22d9ff7967b9e144c2e7babe3169c62c15
Pieter Stokkink <118258379+pieter-winc@users.noreply.github.com>
2023-02-13 13:58:30 +0100

    add solution to the useReducer exercise

---
+++ exercise-hooks/useReducer-exercise/solution/src/AddBookForm.jsx
@@ -0,0 +1,27 @@
+import { useLibrary } from "./LibraryContext";
+
+export const AddBookForm = () => {
+  const {
+    actions: { addBook },
+  } = useLibrary();
+
+  const submitForm = (event) => {
+    event.preventDefault();
+    addBook({
+      title: event.target.elements.title.value,
+      author: event.target.elements.author.value,
+      category: event.target.elements.category.value.toLowerCase(),
+    });
+  };
+  return (
+    <form onSubmit={submitForm}>
+      <label htmlFor="author">author:</label>
+      <input type="text" name="author" />
+      <label htmlFor="title">title:</label>
+      <input type="text" name="title" />
+      <label htmlFor="category">category:</label>
+      <input type="text" name="category" />
+      <button type="submit">+📖 Add Book</button>
+    </form>
+  );
+};
--- exercise-hooks/useReducer-exercise/solution/src/Books.jsx
+++ exercise-hooks/useReducer-exercise/solution/src/Books.jsx
@@ -1,5 +1,6 @@
 import { Category } from "./Category";
 import { useLibrary } from "./LibraryContext";
+import { AddBookForm } from "./AddBookForm";
 
 export const Books = () => {
   const { books } = useLibrary();
@@ -19,6 +20,7 @@ export const Books = () => {
           category={category}
         />
       ))}
+      <AddBookForm />
     </>
   );
 };

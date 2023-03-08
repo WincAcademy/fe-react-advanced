f31ec3484b4ba14390bc83ab25e28d5c8762e864
Mark van der Laan <mrkvdl@gmail.com>
2023-03-08 17:04:45 +0100

    fix: useEffect fetcher in starter files

--- exercise-hooks/useEffect-exercise/starter/starter/src/fetchers.js
+++ exercise-hooks/useEffect-exercise/starter/starter/src/fetchers.js
@@ -27,30 +27,32 @@ export const fetchPeople = async () => {
 
 export const fetchPerson = async (id) => {
   await delay();
-  return [
-    {
-      id: 1,
-      name: "John",
-      age: 42,
-      hobbies: ["Witchcraft", "Magick", "Tarot"],
-    },
-    {
-      id: 2,
-      name: "Jane",
-      age: 74,
-      hobbies: ["Bungy Jumping", "Knitting"],
-    },
-    {
-      id: 3,
-      name: "Bob",
-      age: 18,
-      hobbies: ["Cow herding", "Training dogs"],
-    },
-    {
-      id: 4,
-      name: "Alice",
-      age: 28,
-      hobbies: ["Gardening", "Playing the Banjo"],
-    },
-  ][id];
+  return id
+    ? [
+        {
+          id: 1,
+          name: "John",
+          age: 42,
+          hobbies: ["Witchcraft", "Magick", "Tarot"],
+        },
+        {
+          id: 2,
+          name: "Jane",
+          age: 74,
+          hobbies: ["Bungy Jumping", "Knitting"],
+        },
+        {
+          id: 3,
+          name: "Bob",
+          age: 18,
+          hobbies: ["Cow herding", "Training dogs"],
+        },
+        {
+          id: 4,
+          name: "Alice",
+          age: 28,
+          hobbies: ["Gardening", "Playing the Banjo"],
+        },
+      ].find((p) => p.id === id)
+    : null;
 };

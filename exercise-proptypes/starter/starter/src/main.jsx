d5cc3762b775c0d9a362b6f68a8e6a65f68b801e
Mark van der Laan <mrkvdl@gmail.com>
2023-03-13 14:26:36 +0100

    fix: final project starter import React to avoid PropType warning

--- exercise-proptypes/starter/starter/src/main.jsx
+++ exercise-proptypes/starter/starter/src/main.jsx
@@ -1,21 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Person from "./Person";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Person from './Person';
+import './index.css';
 
 // @ts-ignore
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Person
       name="Johnny"
       age={27}
       address={{
-        street: "Zuckerberg Avenue",
+        street: 'Zuckerberg Avenue',
         houseNumber: 1,
-        city: "San Francisco",
+        city: 'San Francisco',
       }}
-      hobbies={["playing piano", "cooking", "knitting"]}
+      hobbies={['playing piano', 'cooking', 'knitting']}
       pronoun="He"
     />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
--- final-project-starter/starter/src/components/Root.jsx
+++ final-project-starter/starter/src/components/Root.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Navigation } from './Navigation';
 import { Box } from '@chakra-ui/react';
